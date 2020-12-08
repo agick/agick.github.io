37bf9524383ed3a5d05a8d4ffce139be01971fde
agick <andreasgoericke@gmail.com>
2020-12-08 23:17:30 +0100

    added more functionality

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+
+function changeMenuView(e){
+    value = e.target.parentNode.getAttribute("data-value");
+    if(value != null){
+        for (let menucontentitem of e.target.parentNode.parentNode.parentNode.getElementsByClassName('menucontent')[0].getElementsByClassName('menucontentitem')){
+            if(menucontentitem.getAttribute("data-value") == value){
+                menucontentitem.classList.add('menucontentitemselected');
+            } else {
+                menucontentitem.classList.remove('menucontentitemselected');
+            }
+        }
+    }
+}
+
+
+
+window.onload = function(){ 
+    const menubar  = document.querySelector('.menubar');
+    menubar.addEventListener('click', changeMenuView);
+}
